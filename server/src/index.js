87a1303931aecdf4c04cd21775b92e6b90ac32d9
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-04-22 22:38:52 +0300

    feat(apollo-server):

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,37 @@
-//TODO
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const PORT = 5001;
+
+/** const mock = {
+  SpaceCat: () => ({
+    id: () => "spacecat_01",
+    title: () => "spacecat pioneer"
+  })
+} */
+
+const mocks = {
+	Query: () => ({
+		tracksForHome: () => [...new Array(6)],
+	}),
+	Track: () => ({
+		id: () => 'track_01',
+		title: () => 'Astro Kitty, Space Explorer',
+		author: () => {
+			return {
+				name: 'Grumpy Cat',
+				photo:
+					'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+			};
+		},
+		thumbnail: () =>
+			'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+		length: () => 1210,
+		modulesCount: () => 6,
+	}),
+};
+
+const server = new ApolloServer({ typeDefs, mocks });
+
+server.listen(PORT, () =>
+	console.log(`Listening on port ${PORT} \nQuery at https://studio.apollographql.com/dev`)
+);
