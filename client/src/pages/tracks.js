1b924a4d3a6339c0694c8549db2f95c787acdd56
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-04-23 08:58:36 +0300

    feat(defining-a-query):

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+/** === 
+export const SPACECATS = gql`
+  query ListSpaceCats {
+    spaceCats{
+      name
+      age
+      missions{
+        name
+        description
+      }
+    }
+  }
+` === */
+
+export const TRACKS = gql`
+	query getTracks {
+		tracksForHome {
+			id
+			title
+			thumbnail
+			length
+			modulesCount
+			author {
+				id
+				name
+				photo
+			}
+		}
+	}
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+	return <Layout grid> </Layout>;
 };
 
 export default Tracks;
