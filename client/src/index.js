546b82c292b9eaa2cce051a87f887d185ca731e7
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-04-23 08:41:07 +0300

    feat(apollo-client-setup):

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({ uri: 'http://localhost:5001', cache: new InMemoryCache() });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<React.StrictMode>
+			<GlobalStyles />
+			<Pages />
+		</React.StrictMode>
+	</ApolloProvider>,
+	document.getElementById('root')
 );
