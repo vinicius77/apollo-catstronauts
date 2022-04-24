33f0448d08f18d6be72fa96bc639c0121d237043
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-04-24 12:16:19 +0300

    feat(implementing-query-resolvers):

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,7 +1,18 @@
 const resolvers = {
 	Query: {
 		// Returns an array of tracks (Homepage data)
-		trackForHome: (parent, args, context, info) => {},
+		trackForHome: (_parent, _args, context, _info) => {
+			const { dataSources } = context;
+			return dataSources.trackAPI.getTracks();
+		},
+	},
+	Track: {
+		// verbose for learning purposes
+		author: (parent, _args, context, _info) => {
+			const { authorId } = parent;
+			const { dataSources } = context;
+			return dataSources.trackAPI.getAuthor(authorId);
+		},
 	},
 };
 
