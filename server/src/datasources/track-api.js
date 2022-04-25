7cff4f106d8313285a878dec608c9ba06b0f843c
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-04-25 20:37:33 +0300

    feat(querying-live-data): corrects this.baseURL

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -3,7 +3,7 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 class TrackAPI extends RESTDataSource {
 	constructor() {
 		super();
-		this.baseUrl = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+		this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
 	}
 
 	/** Methods used by the resolver(s) */
