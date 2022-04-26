b6e7826ab72a82f7d99871e4a19cacb1667c90ee
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-04-26 20:19:38 +0300

    feat(updating-our-schema):

--- server/src/schema.js
+++ server/src/schema.js
@@ -17,6 +17,15 @@ const typeDefs = gql`
 		length: Int
 		modulesCount: Int
 		author: Author!
+		numberOfViews: Int
+		description: String
+		modules: [Module!]!
+	}
+
+	type Module {
+		id: ID!
+		title: String!
+		length: Int
 	}
 
 	"Well, it is an author of the track"
